bbcd6c2cf7fba5160bb7ed7e2b78d4b4ee0e32c9
Hilde-at-480956491255 <speilbildea@gmail.com>
2018-04-10 10:09:13 +0200

    Enum Values added

--- src/com/rominntrenger/main/maploader/ID.java
+++ src/com/rominntrenger/main/maploader/ID.java
@@ -1,5 +1,24 @@
 package com.rominntrenger.main.maploader;
 
 public enum ID {
+    Wall,
+    Door,
+    StasisBox,
+    KeyY,
+    KeyB,
+    KeyG,
+    KeyP,
+    KeyR,
+    Box,
+    Crate,
+    Corpse,
+    Ship,
+    Table,
+    Bench,
+    AlienGreen,
+    AlienPurple,
+    AlienExplode,
+    AlienTurret,
+    SpawnPlayer
 
 }
