31de79745cee9912a11bdaa1113a68b6a02c12f8
Paal-at-480956491255 <pm96@live.no>
2018-04-19 14:03:27 +0200

    refractored some code and deleted unused files

--- src/com/rominntrenger/objects/FSM/Wander.java
+++ src/com/rominntrenger/objects/FSM/Wander.java
@@ -8,43 +8,37 @@ import com.rominntrenger.objects.player.Player;
 
 public class Wander implements Behaviour {
 
-    private double moveInterval = 0.7;
     private long prevRandomMove = 0;
 
 
     @Override
     public void nextBehaviour(Enemy behaviourContext) {
-        Enemy enemy = behaviourContext;
-        double speed = enemy.getSpeed();
-        double delta = enemy.getDelta();
+        double speed = behaviourContext.getSpeed();
+        double delta = behaviourContext.getDelta();
 
 
-        if (behaviourContext != null) {
+        double moveInterval = 0.7;
+        if ((System.currentTimeMillis() - prevRandomMove) / 1000 >= moveInterval) {
+            prevRandomMove = System.currentTimeMillis();
+            behaviourContext.setSpeed(150);
+            RandomMove(behaviourContext);
+        }
 
-            if ((System.currentTimeMillis() - prevRandomMove) / 1000 >= moveInterval) {
-                prevRandomMove = System.currentTimeMillis();
-                enemy.setSpeed(150);
-                RandomMove(behaviourContext);
-            }
-
-            enemy.translate(Vec2.multiply(enemy.getDirection(), speed * delta));
-            Player player = ((RomInntrenger) GameApplication.getInstance()).player;
-        double distance2Player = enemy.getPosition().distance(player.getPosition());
+        behaviourContext.translate(Vec2.multiply(behaviourContext.getDirection(), speed * delta));
+        Player player = ((RomInntrenger) GameApplication.getInstance()).player;
+        double distance2Player = behaviourContext.getPosition().distance(player.getPosition());
         if (distance2Player <= 750) {
             behaviourContext.setBehaviour(new Attack());
         }
 
     }
-
-    }
-    public void RandomMove(Enemy behaviourContext){
-        Enemy enemy = behaviourContext;
-        double speed = enemy.getSpeed();
-        double delta = enemy.getDelta();
+    private void RandomMove(Enemy behaviourContext){
+        double speed = behaviourContext.getSpeed();
+        double delta = behaviourContext.getDelta();
 
         Vec2 direction = Vec2.Vector2FromAngleInDegrees(Math.random() * 360);
-        enemy.setDirection(direction);
-        enemy.translate(Vec2.multiply(direction, speed * delta));
+        behaviourContext.setDirection(direction);
+        behaviourContext.translate(Vec2.multiply(direction, speed * delta));
 
     }
 }
