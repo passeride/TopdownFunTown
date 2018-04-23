68f2252e3696fcd38b8bcba10ad6d540ce3b9d77
Paal-at-480956491255 <pm96@live.no>
2018-04-23 09:44:18 +0200

    added the soundmenu, with a slider

--- src/com/rominntrenger/gui/GameMenu.java
+++ src/com/rominntrenger/gui/GameMenu.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.scene.Parent;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -12,18 +13,19 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class GameMenu extends Parent {
-        private VBox menu0;
-        private VBox menu1;
+        private VBox menu0, menu1, menu2;
+        private Slider soundSlider;
         private Stage primaryStage;
         private final int offset = 400;
         private Rectangle bg;
-        public MenuButton resumeButton, optionsButton, exitButton, backButton, soundButton, videoButton, goreButton;
+        public MenuButton resumeButton, optionsButton, exitButton, backButtonMainMenu, soundButton, videoButton, goreButton, backButtonSoundMenu;
 
 
     public GameMenu(Stage primaryStage) {
         this.primaryStage = primaryStage;
             menu0 = new VBox(10);
             menu1 = new VBox(10);
+            menu2 = new VBox(10);
 
             menu0.setTranslateX(100);
             menu0.setTranslateY(300);
@@ -31,8 +33,12 @@ public class GameMenu extends Parent {
             menu1.setTranslateX(100);
             menu1.setTranslateY(300);
 
+            menu2.setTranslateX(100);
+            menu2.setTranslateY(300);
+
 
             menu1.setTranslateX(offset);
+            menu2.setTranslateX(offset);
 
             addButton();
 
@@ -70,16 +76,16 @@ public class GameMenu extends Parent {
             optionsButton.setOnMouseClicked(event -> {
                 getChildren().add(menu1);
 
-                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), menu0);
-                tt.setToX(menu0.getTranslateX() - offset);
+                TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.25), menu0);
+                translateTransition.setToX(menu0.getTranslateX() - offset);
 
-                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), menu1);
-                tt1.setToX(menu0.getTranslateX());
+                TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.5), menu1);
+                translateTransition1.setToX(menu0.getTranslateX());
 
-                tt.play();
-                tt1.play();
+                translateTransition.play();
+                translateTransition1.play();
 
-                tt.setOnFinished(evt -> {
+                translateTransition.setOnFinished(evt -> {
                     getChildren().remove(menu0);
                 });
             });
@@ -89,8 +95,8 @@ public class GameMenu extends Parent {
                 System.exit(0);
             });
 
-            backButton = new MenuButton("BACK");
-            backButton.setOnMouseClicked(event -> {
+            backButtonMainMenu = new MenuButton("BACK");
+            backButtonMainMenu.setOnMouseClicked(event -> {
                 getChildren().add(menu0);
 
                 TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), menu1);
@@ -107,12 +113,52 @@ public class GameMenu extends Parent {
                 });
             });
 
-             soundButton = new MenuButton("SOUND");
-             videoButton = new MenuButton("VIDEO");
-             goreButton = new MenuButton("GORE");
+            soundButton = new MenuButton("SOUND");
+            soundButton.setOnMouseClicked(event -> {
+                getChildren().add(menu2);
+
+                TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.25), menu1);
+                translateTransition.setToX(menu1.getTranslateX() - offset);
+
+                TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.5), menu2);
+                translateTransition1.setToX(menu1.getTranslateX());
+
+                translateTransition.play();
+                translateTransition1.play();
+
+                translateTransition.setOnFinished(evt -> {
+                    getChildren().remove(menu1);
+                });
+            });
+            backButtonSoundMenu = new MenuButton("BACK");
+            backButtonSoundMenu.setOnMouseClicked(event -> {
+                getChildren().add(menu1);
+
+                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), menu2);
+                tt.setToX(menu2.getTranslateX() + offset);
+
+                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), menu1);
+                tt1.setToX(menu2.getTranslateX());
+
+                tt.play();
+                tt1.play();
+
+                tt.setOnFinished(evt -> {
+                    getChildren().remove(menu2);
+                });
+            });
+            soundSlider = new Slider();
+            soundSlider.setMin(0);
+            soundSlider.setMax(100);
+            soundSlider.setValue(100);
+
+
+            videoButton = new MenuButton("VIDEO");
+            goreButton = new MenuButton("GORE");
 
             menu0.getChildren().addAll(resumeButton, optionsButton, exitButton);
-            menu1.getChildren().addAll(backButton, soundButton, videoButton, goreButton);
+            menu1.getChildren().addAll(backButtonMainMenu, soundButton, videoButton, goreButton);
+            menu2.getChildren().addAll(backButtonSoundMenu, soundSlider);
         }
 
 
